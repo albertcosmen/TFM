2d1194b9796db88bcd8d1496a625ed3c1adbb79b
albertcosmen <albertcosmen@gmail.com>
2025-05-18 09:31:20 +0200

    Update terrorism_app.py

--- terrorism-app/terrorism_app.py
+++ terrorism-app/terrorism_app.py
@@ -334,7 +334,7 @@ def pie_most_active_groups (df,country):
 
 
 
-df = load_data("Terrorism_clean_dataset.csv")
+df = load_data("https://drive.google.com/file/d/1I4X4xg2ynkr7AoVhSYJVRATrSWPN_7TY/view?usp=sharing")
 
 
 st.title("Global Terrorism Exploration APP")
